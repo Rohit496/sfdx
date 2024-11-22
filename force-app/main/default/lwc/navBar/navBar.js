f5927b924ea3e9cc52bae6f4313ecc2e8a935b0e
Rohit Kumar <rohitdotnet@outlook.com>
2024-11-22 09:33:33 +0530

    Update navBar.js

--- force-app/main/default/lwc/navBar/navBar.js
+++ force-app/main/default/lwc/navBar/navBar.js
@@ -3,5 +3,6 @@ import { LightningElement } from 'lwc';
 export default class NavBar extends LightningElement {
   connectedCallback(){
   console.log('Hello NavBar');
+  console.log('Hello Nav bar gain :)');
 }
 }
