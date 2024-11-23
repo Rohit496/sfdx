5706130a93f5de5880bb95010d176d170c87a8c4
Rohit <rohitdotnet@outlook.com>
2024-11-23 09:46:39 +0530

    Add constructor to GitDemo component with console log messages

--- force-app/main/default/lwc/gitDemo/gitDemo.js
+++ force-app/main/default/lwc/gitDemo/gitDemo.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class GitDemo extends LightningElement {}
+export default class GitDemo extends LightningElement {
+    constructor() {
+        super();
+        console.log('Hello GitDemo');
+        console.log('Hello GitDemo again');
+    }
+}
